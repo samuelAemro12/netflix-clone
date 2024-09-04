1d2f616f2b19bd252fda9b82653c52700f65e980
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-04 19:59:56 +0300

    added the base code for components file, like footer and TitleCards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from './Pages/Home/Home';
+import Home from './Pages/Home/Home.js';
 
 const App = () => {
   return (
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Home.css';
-import NavigationBar from '../../componets/NavigationBar/NavigationBar/';
+
 const Home = () => {
   return (
     <div className='home'>
-      <NavigationBar/>
+    
     </div>
   );
 }
