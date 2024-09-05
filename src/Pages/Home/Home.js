e9e57937e3b88af08d034456f681805bce442454
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-05 22:37:04 +0300

    added footer component to the home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Home.css';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   return (
     <div className='home'>
       <NavigationBar/>
-
+    <Footer/>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,13 +4,45 @@ import facebookLogo from '../../files/facebook_icon.png';
 import instagramLogo from '../../files/instagram_icon.png';
 import youtubeLogo from '../../files/youtube_icon.png';
 
-
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="outer-container">
+    <div className="inner-container">
+        <div>
+            <img src={facebookLogo} alt="facebook logo"/>
+            <img src={instagramLogo} alt="instagram logo"/>
+            <img src={youtubeLogo} alt="youTube logo"/>
+         </div>
+        < div className="footer-data">
+                <ul>
+                    <li>Audio Description</li>
+                    <li>Investor Relations</li>
+                    <li>Legal Notice</li>
+                </ul>
+                <ul>
+                    <li>Help Center</li>
+                    <li>Jobs</li>
+                    <li>Cookie Preferences</li>
+                </ul>
+                <ul>
+                    <li>Gift Cards</li>
+                    <li>Terms of Use</li>
+                    <li>Corporate Information</li>
+                </ul>
+                <ul>
+                    <li>Media Center</li>
+                    <li>Privacy</li>
+                    <li>Contact Us</li>
+                </ul>
+        </div>
+        <div className="service-code">
+            <p>Service Code</p>
+        </div>
+        <div className="copy-write">
+            &copy; 1997-2024 Netflix, Inc.
+        </div>
     </div>
-  )
+</div>
+  );
 }
-
-export default Footer
+export default Footer;
