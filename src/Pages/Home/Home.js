14194bd3e619feb79e60c33c135e217144018e01
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-05 21:56:34 +0300

    fixed the files folders location and the navigation bar component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
 
 const Home = () => {
   return (
     <div className='home'>
-    
+      <NavigationBar/>
+      
     </div>
   );
 }
