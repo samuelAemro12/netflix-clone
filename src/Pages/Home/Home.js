3d38c06a9de91d58157d19660102264443224d04
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-06 20:58:19 +0300

    added the home page's xml struture and imported some files images

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,30 @@ import React from 'react';
 import './Home.css';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Footer from '../../components/Footer/Footer';
+import HomeImageBanner from '../../files/image-banner.jpg';
+import ImageBannerTitle from '../../files/image-banner-title.png';
+import playImage from '../../files/play_icon.png';
+import InfoImage from '../../files/info_icon.png';
 
 const Home = () => {
   return (
     <div className='home'>
       <NavigationBar/>
-    <Footer/>
+      <div className='banner-home'>
+                <img src={HomeImageBanner} alt='' className='banner-image'/>
+                <div className='banner-caption'>
+                    <img src={ImageBannerTitle} alt='' className='caption-image'/>
+                    <p>Discovering his ties to a secret ancient order, a young man living 
+                        in modern istanbul embarks on a quest to save the city from an 
+                        immortal enemy.
+                    </p>
+                    <div className='home-buttons'>
+                        <button className='home-button'><img src={playImage} alt=''/>Play</button>
+                        <button className='home-button dark-button'><img src={InfoImage} alt=''/>More Info</button>
+                    </div>
+                </div>
+          </div>
+      <Footer/>
     </div>
   );
 }
