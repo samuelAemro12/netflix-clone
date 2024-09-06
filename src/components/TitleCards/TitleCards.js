f145fbec61140134726ced48aaeb14ea720323cd
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-06 21:29:40 +0300

    fixed the TitleCards rendering though lacking styling

--- src/components/TitleCards/TitleCards.js
+++ src/components/TitleCards/TitleCards.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
 import Movie_Data from "../../files/MovieRendering/MovieRendering";
 
-const TitleCards = () => {
-  return (
-    <div className="titleCards">
-      <h2>{title? title : "Popular on Netflix"}</h2>
-      <div className="movie-list" ref={MoviesRef}>
-      {Movie_Data.map((movie, index)=>{
-            return(
-            <div className="Movie" key={index}>
-                <img src={movie.image} alt=""/>
-                <p src={movie.name} alt=""/>
-            </div>
-            );
-        })}
-    </div>
-  </div>
-  );
-}
+const TitleCards = ({title, category }) =>{
+    const MoviesRef = useRef();
+    const handleWheel = event =>{
+        event.preventDefault();
+        MoviesRef.current.scrollLeft += event.deltaY;
+    }
+    useEffect(()=>{
+        MoviesRef.current.addEventListener('wheel', handleWheel);
+    },[]);
 
+    return(
+        <div className="titleCards">
+            <h2>{title? title : "Popular on Netflix"}</h2>
+            <div className="movie-list" ref={MoviesRef}>
+                {Movie_Data.map((movie, index)=>{
+                    return(
+                    <div className="Movie" key={index}>
+                        <img src={movie.image} alt=""/>
+                        <p src={movie.name} alt=""/>
+                    </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+
+}
 export default TitleCards;
