50757e4e9be91cffc7eff5a9c22f492be57ff7b8
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-09 01:26:31 +0300

    succussfully mounted the login page, at /login and used the Routes avd Routes compos from the react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Home from './Pages/Home/Home.js';
+import { Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login/Login.js';
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path='/' element={ <Home/>}/>
+        <Route path='/login' element={ <Login/>}/>
+      </Routes>
     </div>
   )
 }
