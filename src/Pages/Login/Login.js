8ed8230bd7123c03fd6c053155fc12663017eb85
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-09 01:09:38 +0300

    added react router dependency so that i can route between webpages

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
@@ -8,4 +8,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
