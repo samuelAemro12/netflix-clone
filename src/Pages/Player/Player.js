a45b1980d19cb1e0047d6abcbef82e8aee054ed7
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-10 01:24:44 +0300

    styling and jsx of player.js is done for the now

--- src/Pages/Player/Player.js
+++ src/Pages/Player/Player.js
@@ -3,11 +3,11 @@ import backArrow from '../../files/back_arrow_icon.png';
 
 const Player = () => {
   return (
-    <div>
+    <div className='player-page'>
       <img src={backArrow} alt='back arrow icon'/>
       <iframe width='85%' height='85%' src='{}https://www.youtube.com/embed/...' title='Trailer'
       frameBorder='0' allowFullScreen></iframe>
-      <div className=''>
+      <div className='player-information'>
         <p>Published Date</p>
         <p>Name</p>
         <p>Type</p>
