20869bf402251aa11dc6efd211bfa8ce66781f32
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-09 15:33:20 +0300

    added the xml structure for the login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import './Login.css';
+import netflixLogo from '../../files/netflixLogo.png';
 
 const Login = () => {
   return (
     <div className=''>
-      
+      <img src={netflixLogo} alt='' className='netflixLogoImage'/>
+      <div className=''>
+        <h1>Sign Up</h1>
+        <form>
+          <input type='text' placeholder='your name'/>
+          <input type='email' placeholder='Email'/>
+          <input type='password' placeholder='Password'/>
+          <button>Sign Up</button>
+          <div className='form-help'>
+            <div className=''>
+              <input type='checkbox'/>
+              <label htmlFor=''>Remember Me</label>
+            </div>
+            <p>HElP?</p>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login;
