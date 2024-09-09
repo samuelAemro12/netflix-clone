8447e6baee9b0a5d1dc56069041738a6689d8716
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-10 00:00:11 +0300

    completed the jsx structure for the player page

--- src/Pages/Player/Player.js
+++ src/Pages/Player/Player.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import backArrow from '../../files/back_arrow_icon.png';
 
 const Player = () => {
   return (
     <div>
-      
+      <img src={backArrow} alt='back arrow icon'/>
+      <iframe width='85%' height='85%' src='{}https://www.youtube.com/embed/...' title='Trailer'
+      frameBorder='0' allowFullScreen></iframe>
+      <div className=''>
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
     </div>
   )
 }
