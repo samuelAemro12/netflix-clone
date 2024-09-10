cd7b4b6873a0b61289e839027f6511aee178ee72
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-10 03:02:34 +0300

    enabled the palyer page to fetch trailers from youtube based on the key dynamically alloted by the movie databate after a fetch request had been made, the keys are being updated based on the request

--- src/Pages/Player/Player.js
+++ src/Pages/Player/Player.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import backArrow from '../../files/back_arrow_icon.png';
+import { useParams } from 'react-router-dom';
 
 const Player = () => {
+  const [movieApiData, setMovieApiData] = useState({
+    name:"",
+    key:"",
+    publilshed_at:"",
+    typeof:""
+  });
+  const {id} = useParams();
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZTc4NDg4YTA4MTg2ZDUyMDcyMDA3Y2FjNWY1NWIwZSIsIm5iZiI6MTcyNTkyMTM0Ny4zNDIwMzYsInN1YiI6IjY2YzI3ZGVkZTUwNTA5NDZkMTFhOGZiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AgQZA-5J4Ck3ml-kE5sohJp8-1IGQaIEk3f8t30L-sk'
+    }
+  };
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setMovieApiData(response.results[0]))
+    .catch(err => console.error(err));
+  },[]);
+ 
   return (
     <div className='player-page'>
       <img src={backArrow} alt='back arrow icon'/>
-      <iframe width='85%' height='85%' src='{}https://www.youtube.com/embed/...' title='Trailer'
+      <iframe width='85%' height='85%' src ={`https://www.youtube.com/embed/fIOph60LEBA${movieApiData.key}`} title='Trailer'
       frameBorder='0' allowFullScreen></iframe>
       <div className='player-information'>
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{movieApiData.publilshed_at.slice(0,10)}</p>
+        <p>{movieApiData.name}</p>
+        <p>{movieApiData.Type}</p>
       </div>
     </div>
   )
