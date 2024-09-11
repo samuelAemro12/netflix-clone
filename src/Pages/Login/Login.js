ee584388195ac604a5633d4993eac746c0af659a
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-12 02:35:29 +0300

    successfully connected to the firebase database and signin up for the database

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,26 @@
 import React, { useState} from 'react';
 import './Login.css';
 import netflixLogo from '../../files/netflixLogo.png';
+import { login, signup } from '../../firebase';
 
 const Login = () => {
   const [signedIn, setSignedIn] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
   const signState = () =>{setSignedIn("Sign Up");}
   const signStates = () =>{setSignedIn("Sign In");}
+
+  const user_auth = async (event)=>{
+    event.preventDefault();
+    if(signState === "Sign In"){
+      await login(email, password);
+    }else{
+      await signup(name, email, password);
+    }
+  }
 
   return (
     <div className='login'>
@@ -13,10 +28,11 @@ const Login = () => {
       <div className='form-body'>
         <h1>{signedIn}</h1>
         <form>
-          {signedIn === "Sign Up" ? <input type='text' placeholder='Name'/> : <> </>}          
-          <input type='email' placeholder='Email'/>
-          <input type='password' placeholder='Password'/>
-          <button>{signedIn}</button>
+          {signedIn === "Sign Up" ? <input type='text'value={name} onChange={(e)=>{setName(e.target.value) }}
+           placeholder='Name'/> : <> </>}          
+          <input type='email' value={email} onChange={(e)=>{setEmail(e.target.value) }} placeholder='Email'/>
+          <input type='password' value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='Password'/>
+          <button onClick={user_auth} type='submit'>{signedIn}</button>
           <div className='form-help'>
             <div className='save-remember-me'>
               <input type='checkbox'/>
