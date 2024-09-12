bca6a25ef12cae95dce0aac05cdaa55729bc79e9
samuelAemro12 <samuelaemrowork12@gmail.com>
2024-09-13 02:25:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './Pages/Home/Home.js';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './Pages/Login/Login.js';
 import Player from './Pages/Player/Player.js';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase.js';
 
 const App = () => {
+  const navigation = useNavigate();
+  useEffect(()=>{
+    onAuthStateChanged(auth, async(user)=>{
+        if(user){
+          console.log("logged in");
+          navigation('/');
+        }else{
+          console.log("logged out");
+          navigation('/login');
+        }
+    })
+  },[])
   return (
     <div>
       <Routes>
